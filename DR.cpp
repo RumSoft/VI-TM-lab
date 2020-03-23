170d461f68a0cc7c5bffe671002de13e293081d6
Dominik Ruman <d@ruman.dev>
2020-03-23 02:34:10 +0100

    zad3

--- DR.cpp
+++ DR.cpp
@@ -8,74 +8,77 @@
 using namespace std;
 using namespace cv;
 
-bool Diffuse(Mat& img1, Mat& img2, Mat& outputImg, float ratio);
+bool Diffuse(Mat_<uchar>& img1, Mat_<uchar>& img2, Mat_<uchar>& outputImg, float ratio);
+float Clamp(float value, float min, float max);
 
 #define WINDOW_NAME "przenikanie"
 #define KEY_ESC 27
 #define KEY_ENTER 13
+#define KEY_UP 2490368
+#define KEY_DOWN 2621440
 
+float change = 0.02f;
 int main()
 {
 	// Stworzenie okna w którym przechwycone obrazy będą wyświetlane
 	cvNamedWindow(WINDOW_NAME, CV_WINDOW_AUTOSIZE);
 
 	// Pobranie obrazu
-	Mat imagePeppers = imread("peppers.jpg", CV_LOAD_IMAGE_COLOR);
-	Mat imageFruits = imread("fruits.jpg", CV_LOAD_IMAGE_COLOR);
+	Mat im1 = imread("peppers.jpg", CV_LOAD_IMAGE_COLOR);
+	Mat im2 = imread("fruits.jpg", CV_LOAD_IMAGE_COLOR);
 
 	// Utworzenie obiektu przechowującego obraz, który będzie wynikiem przenikania
-	Mat imagesDiffusion = imagePeppers.clone();
+	Mat im3 = im1.clone();
 
+	Mat_<uchar> im1pix = im1;
+	Mat_<uchar> im2pix = im2;
+	Mat_<uchar> im3pix = im3;
 		
 	auto forceStop = false;
+	float ratio = .0f;
+	
 	do
 	{
-		auto ratio = 1.0f;
-		auto change = 0.02f;
-		for (ratio = .0f; ratio <= 1; ratio += change)
-		{
-			if (Diffuse(imagePeppers, imageFruits, imagesDiffusion, ratio) == true)
-				goto end;
-
-			imshow(WINDOW_NAME, imagesDiffusion);
-		}
-		for (ratio = 1.0f; ratio >= 0; ratio -= change)
-		{
-			if (Diffuse(imagePeppers, imageFruits, imagesDiffusion, ratio) == true)
-				goto end;
-
-			imshow(WINDOW_NAME, imagesDiffusion);
-		}
+		ratio = Clamp(ratio + change, 0, 1);
+		if (ratio >= 1 || ratio <= 0)
+			change = -change;
+		
+		forceStop = Diffuse(im1pix, im2pix, im3pix, ratio);		
+		imshow(WINDOW_NAME, im3);
 	}
 	while (!forceStop);
-
-end:
 
 	// Niszczenie okna
 	cvDestroyWindow(WINDOW_NAME);
 	return 0;
 }
 
-bool Diffuse(Mat& img1, Mat& img2, Mat& outputImg, float ratio)
+//hard-limit wartosci na wyjsciu z funkcji miedzy min i max
+float Clamp(float value, float min, float max)
+{
+	if (value >= max) return max;
+	if (value <= min) return min;
+	return value;
+}
+
+bool Diffuse(Mat_<uchar>& img1, Mat_<uchar>& img2, Mat_<uchar>& outputImg, float ratio)
 {
 	for (auto y = 0; y < img1.rows; y++)
-		for (auto x = 0; x < img1.cols; x++) {
-			auto im1pix = img1.at<Vec3b>(y,x);
-			auto im2pix = img2.at<Vec3b>(y,x);
-			auto outpix = outputImg.at<Vec3b>(y, x);
-
-			outpix[0] = im1pix[0] * ratio + im2pix[0] * (1 - ratio);
-			outpix[1] = im1pix[1] * ratio + im2pix[1] * (1 - ratio);
-			outpix[2] = im1pix[2] * ratio + im2pix[2] * (1 - ratio);
-			outputImg.at<Vec3b>(y, x) = outpix;
-		}
-
-	auto key = cvWaitKey(1);
+		for (auto x = 0; x < img1.cols; x++)
+			outputImg[y][x] = img1[y][x] * ratio + img2[y][x] * (1 - ratio);
+		
+	const auto key = waitKeyEx(1);
 	switch (key)
 	{
 	case KEY_ENTER:
 	case KEY_ESC:
 		return true;
+	case KEY_UP:
+		change = Clamp(change * 1.1f, 0.001f, 0.1);
+		break;
+	case KEY_DOWN:
+		change = Clamp(change * 0.9f, 0.001f, 0.1);
+		break;
 	}
 
 	return false;
