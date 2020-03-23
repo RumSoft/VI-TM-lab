dce3376ad840fc83b78b57a9335305911cd8513b
Dominik Ruman <d@ruman.dev>
2020-03-23 01:30:06 +0100

    zad1

--- DR.cpp
+++ DR.cpp
@@ -1,20 +1,78 @@
-// DR.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿#include <opencv2/imgproc/imgproc.hpp>  // Gaussian Blur
+#include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
+#include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+#include <math.h>
 #include <iostream>
+
+using namespace std;
+using namespace cv;
+
+bool Diffuse(Mat_<uchar>& img1, Mat_<uchar>& img2, Mat_<uchar>& outputImg, float ratio);
+
+#define WINDOW_NAME "przenikanie"
+#define KEY_ESC 27
+#define KEY_ENTER 13
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Stworzenie okna w którym przechwycone obrazy będą wyświetlane
+	cvNamedWindow(WINDOW_NAME, CV_WINDOW_AUTOSIZE);
+
+	// Pobranie obrazu
+	Mat imagePeppers = imread("peppers.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat imageFruits = imread("fruits.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+
+	// Utworzenie obiektu przechowującego obraz, który będzie wynikiem przenikania
+	Mat imagesDiffusion = imagePeppers.clone();
+
+	// Uzyskanie macierzy pikseli na podstawie obiektów Mat
+	Mat_<uchar> peppersPixels = imagePeppers;
+	Mat_<uchar> fruitsPixels = imageFruits;
+	Mat_<uchar> imagesDiffusionPixels = imagesDiffusion;
+
+	auto forceStop = false;
+	do
+	{
+		auto ratio = 1.0f;
+		auto change = 0.02f;
+		for (ratio = .0f; ratio <= 1; ratio += change)
+		{
+			if (Diffuse(peppersPixels, fruitsPixels, imagesDiffusionPixels, ratio) == true)
+				goto end;
+
+			imshow(WINDOW_NAME, imagesDiffusion);
+		}
+		for (ratio = 1.0f; ratio >= 0; ratio -= change)
+		{
+			if (Diffuse(peppersPixels, fruitsPixels, imagesDiffusionPixels, ratio) == true)
+				goto end;
+
+			imshow(WINDOW_NAME, imagesDiffusion);
+		}
+	}
+	while (!forceStop);
+
+end:
+
+	// Niszczenie okna
+	cvDestroyWindow(WINDOW_NAME);
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool Diffuse(Mat_<uchar>& img1, Mat_<uchar>& img2, Mat_<uchar>& outputImg, float ratio)
+{
+	for (auto x = 0; x < img1.rows; x++)
+		for (auto y = 0; y < img1.cols; y++)
+			outputImg[x][y] = img1[x][y] * ratio + img2[x][y] * (1 - ratio);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	auto key = cvWaitKey(1);
+	switch (key)
+	{
+	case KEY_ENTER:
+	case KEY_ESC:
+		return true;
+	}
+
+	return false;
+}
